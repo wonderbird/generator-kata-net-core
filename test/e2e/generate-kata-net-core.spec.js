93990d161b2b8ffd10c10e01a8e636fadf558b7a
Stefan Boos <kontakt@boos.systems>
2020-07-07 06:40:08 +0200

    refactor e2e test

--- test/e2e/generate-kata-net-core.spec.js
+++ test/e2e/generate-kata-net-core.spec.js
@@ -5,45 +5,82 @@ var helpers = require('yeoman-test');
 
 describe('yo kata-net-core',
     function () {
-        beforeEach(function () {
-            this.timeout(20000);
 
-            var _self = this;
-
-            const visualStudioProjectExtension = '.csproj';
-            const libraryProjectDir = 'SampleKata.Lib';
-            const libraryProjectFile = libraryProjectDir + visualStudioProjectExtension;
-            const defaultClassFileInEmptyProject = 'Class1.cs';
-
-            this.directoriesToCreate = [
-                libraryProjectDir
-            ];
-
-            this.filesToCreate = [
-                path.join(libraryProjectDir, libraryProjectFile),
-                path.join(libraryProjectDir, defaultClassFileInEmptyProject)
-            ];
-
-            return helpers.run(path.join(__dirname, '../../app'))
-                .then(function(dir) {
-                    _self.tmpDir = dir;
-                });
-        });
-
-        afterEach(function () {
-            this.directoriesToCreate.forEach(relativePath => {
-                var pathToDelete = path.join(this.tmpDir, relativePath);
-
-                try {
-                    fs.rmdirSync(pathToDelete, { recursive: true });
-                } catch (err) {
-                    console.error('Error while deleting ' + pathToDelete);
-                }
-            });
-        });
-
-        it('should create required directories and files',
+        it('should create required files and directories',
             function () {
-                assert.file(this.filesToCreate);
+                const testContext = new GenerateKataNetCoreTestContext(this);
+
+                return testContext.runGeneratorUnderTest()
+                    .then(function () {
+                        const expectedFiles = testContext.getExpectedFiles();
+
+                        assert.file(expectedFiles);
+
+                        testContext.deleteTestExecutionFolderContents();
+                    });
             });
     });
+
+class GenerateKataNetCoreTestContext {
+    constructor(mocha) {
+        this.mocha = mocha;
+
+        const visualStudioProjectExtension = '.csproj';
+        const visualStudioProjectName = 'SampleKata.Lib';
+
+        this.runGeneratorUnderTest = function () {
+            this.configureTestExecutionTimeout();
+
+            var self = this;
+            const rememberTestExecutionDirectoryCallback = function (path) {
+                self.testExecutionDirectory = path;
+            }
+
+            return helpers.run(path.join(__dirname, '../../app'))
+                .then(rememberTestExecutionDirectoryCallback);
+        };
+
+        this.configureTestExecutionTimeout = function () {
+            const averageTestExecutionDurationOnGithubInMilliseconds = 12000;
+            const safetyTimeoutMarginInMilliseconds = 5000;
+            const testExecutionTimeoutInMilliseconds =
+                averageTestExecutionDurationOnGithubInMilliseconds + safetyTimeoutMarginInMilliseconds;
+
+            this.mocha.timeout(testExecutionTimeoutInMilliseconds);
+        };
+
+        this.getExpectedFiles = function () {
+            const classFileName = 'Class1.cs';
+            const projectFileName = visualStudioProjectName + visualStudioProjectExtension;
+
+            return [
+                path.join(visualStudioProjectName, classFileName),
+                path.join(visualStudioProjectName, projectFileName)
+            ];
+        };
+
+        this.deleteTestExecutionFolderContents = function () {
+            const self = this;
+
+            const expectedDirectories = this.getExpectedDirectories();
+            expectedDirectories.forEach(relativePath => {
+                var pathToDelete = path.join(self.testExecutionDirectory, relativePath);
+                self.deleteFileOrDirectoryRecursively(pathToDelete);
+            });
+        };
+
+        this.getExpectedDirectories = function () {
+            return [
+                visualStudioProjectName
+            ];
+        };
+
+        this.deleteFileOrDirectoryRecursively = function (pathToDelete) {
+            try {
+                fs.rmdirSync(pathToDelete, { recursive: true });
+            } catch (err) {
+                console.error('Error while deleting ' + pathToDelete);
+            }
+        };
+    }
+}
