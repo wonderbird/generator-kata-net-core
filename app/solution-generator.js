6a510873b431cc37073001defb0184ee341011ee
Stefan Boos <sboos@igus.net>
2020-07-27 18:13:54 +0200

    refactoring

--- app/solution-generator.js
+++ app/solution-generator.js
@@ -3,21 +3,35 @@ module.exports = class SolutionGenerator {
         this.yeoman = yeoman;
     }
 
-    generate() {
+    generateSolution() {
         this.yeoman.log('Creating .NET Core solution ...');
         this.yeoman.spawnCommandSync('dotnet',
             ['new', 'sln', '--output', 'SampleKata']);
+    }
 
+    generateClassLibrary() {
         process.chdir('SampleKata');
 
         this.yeoman.log('Creating .NET Core class library ...');
         this.yeoman.spawnCommandSync('dotnet',
             ['new', 'classlib', '--language', 'C#', '--name', 'SampleKata.Lib']);
 
+        process.chdir('..');
+    }
+
+    addClassLibraryToSolution() {
+        process.chdir('SampleKata');
+
         this.yeoman.log('Adding .NET Core class library to solution ...');
         this.yeoman.spawnCommandSync('dotnet',
             ['sln', 'add', 'SampleKata.Lib/SampleKata.Lib.csproj']);
 
         process.chdir('..');
+    }
+
+    generate() {
+        this.generateSolution();
+        this.generateClassLibrary();
+        this.addClassLibraryToSolution();
     }
 }
