4d0d9e804f7acc59c71d987d24ca5569b5c1a446
Stefan Boos <sboos@igus.net>
2020-08-12 07:36:01 +0200

    fix: timeout in e2e test

--- test/e2e/generate-kata-net-core.spec.js
+++ test/e2e/generate-kata-net-core.spec.js
@@ -31,7 +31,7 @@ describe('yo kata-net-core',
         var expectedFiles = [];
 
         function configureTestExecutionTimeout(mochaContext) {
-            const maxTestExecutionDurationOnGithubInMilliseconds = 37000;
+            const maxTestExecutionDurationOnGithubInMilliseconds = 45000;
             const safetyTimeoutMarginInMilliseconds = 5000;
             const testExecutionTimeoutInMilliseconds =
                 maxTestExecutionDurationOnGithubInMilliseconds + safetyTimeoutMarginInMilliseconds;
