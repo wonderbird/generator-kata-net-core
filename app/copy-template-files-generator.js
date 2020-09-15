fdb710899cff91e4e9831524ab7732dca5b2b271
Stefan Boos <stefan.boos@gmx.de>
2020-09-15 05:28:05 +0200

    feat(dupfinder): refactoring CopyTemplateFilesGenerator

--- app/copy-template-files-generator.js
+++ app/copy-template-files-generator.js
@@ -4,31 +4,21 @@ module.exports = class CopyTemplateFilesGenerator {
     constructor(fileSystem, configuration) {
         this.fileSystem = fileSystem;
         this.configuration = configuration;
+
+        this.sourceAndDestinationRelativePaths = [
+            { source: 'README.md', destination: 'README.md' },
+            { source: 'gitignore', destination: '.gitignore' },
+            { source: path.join('tools', 'msxsl.exe'), destination: path.join('tools', 'msxsl.exe') },
+        ]
     }
 
-    _copyReadme() {
-        const sourceFileName = 'README.md';
-        const destinationPath = path.join(this.configuration.solutionDirectory, sourceFileName);
-        this.fileSystem.copyTemplate(sourceFileName, destinationPath);
-    }
-
-    _copyGitignore() {
-        const sourceFileName = 'gitignore';
-        const destinationFileName = '.gitignore';
-        const destinationPath = path.join(this.configuration.solutionDirectory, destinationFileName);
-        this.fileSystem.copyTemplate(sourceFileName, destinationPath);
-    }
-    
-    _copyMsxslExe() {
-        const sourceFileName = 'msxsl.exe';
-        const sourcePath = path.join('tools', sourceFileName);
-        const destinationPath = path.join(this.configuration.solutionDirectory, 'tools', sourceFileName);
-        this.fileSystem.copyTemplate(sourcePath, destinationPath);
+    _copy(sourceRelativePath, destinationRelativePath) {
+        const destinationPath = path.join(this.configuration.solutionDirectory, destinationRelativePath);
+        this.fileSystem.copyTemplate(sourceRelativePath, destinationPath);
     }
 
     generate() {
-        this._copyReadme()
-        this._copyGitignore()
-        this._copyMsxslExe();
+        this.sourceAndDestinationRelativePaths.forEach(
+            sourceAndDestination => this._copy(sourceAndDestination.source, sourceAndDestination.destination));
     }
 }
